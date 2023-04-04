001d05940e831b122e09eafb882c4d994daca26a
Dwayne Dreakford <dwayne.dreakford@sealights.io>
2023-04-04 13:36:16 -0400

    Apply SL CD agent at application deployment (without modification to target service image).

--- monitor_java_cd.sh
+++ monitor_java_cd.sh
@@ -1,23 +1,4 @@
 #!/bin/sh
 
-# Ensure there are no conflicts with what other VMs may
-# need from JAVA_TOOL_OPTIONS by stashing, clearing, and
-# then replacing its value after we're done here.
-SAVE_JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS}"
-JAVA_TOOL_OPTIONS=
-
 # Scan and monitor the app on deployment
-java -jar -javaagent:$LIBS_DIR/sl-cd-agent/sl-cd-agent.jar \
-    -Dsl.tokenFile=$LIBS_DIR/sl-cd-agent/sltoken.txt \
-    -Dsl.appName=$SL_COMPONENT_NAME \
-    -Dsl.buildName=$SL_COMPONENT_BUILD \
-    -Dsl.labId=$SL_LAB_ID \
-    -Dsl.includes="com.slsamples.gradle.java.springboot*" \
-    -Dsl.workspace="$LIBS_DIR" \
-    -Dsl.log.level=info \
-    -Dsl.log.toConsole=true \
-    -Dsl.log.toFile=true -Dsl.log.folder=$LIBS_DIR/sl-cd-agent \
-    $LIBS_DIR/rest-calculator-0.2.0-SNAPSHOT.jar \
-    com.slsamples.gradle.java.springboot.Application
-
-JAVA_TOOL_OPTIONS="${SAVE_JAVA_TOOL_OPTIONS}"
+java -jar $LIBS_DIR/rest-calculator-0.2.0-SNAPSHOT.jar com.slsamples.gradle.java.springboot.Application
